e2afc11b6f2b8b3a5dc3d4e80f6ad1a952be8940
1361579531@qq.com <1361579531@qq.com>
2016-05-17 18:18:11 +0800

    add addTextToImage

---
+++ addTextToImage/addTextToImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  addTextToImage
+//
+//  Created by Linyou-IOS-1 on 16/5/17.
+//  Copyright © 2016年 Linyou-IOS-1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ addTextToImage/addTextToImage/ViewController.m
@@ -0,0 +1,68 @@
+//
+//  ViewController.m
+//  addTextToImage
+//
+//  Created by Linyou-IOS-1 on 16/5/17.
+//  Copyright © 2016年 Linyou-IOS-1. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+-(UIImage *)addText:(UIImage *)img text:(NSString *)text1
+{
+    //上下文的大小
+    int w = img.size.width;
+    int h = img.size.height;
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();//创建颜色
+    //创建上下文
+    CGContextRef context = CGBitmapContextCreate(NULL, w, h, 8, 44 * w, colorSpace, kCGImageAlphaPremultipliedFirst);
+    CGContextDrawImage(context, CGRectMake(0, 0, w, h), img.CGImage);//将img绘至context上下文中
+    CGContextSetRGBFillColor(context, 0.0, 1.0, 1.0, 1);//设置颜色
+    char* text = (char *)[text1 cStringUsingEncoding:NSASCIIStringEncoding];
+    CGContextSelectFont(context, "Georgia", 30, kCGEncodingMacRoman);//设置字体的大小
+    CGContextSetTextDrawingMode(context, kCGTextFill);//设置字体绘制方式
+    CGContextSetRGBFillColor(context, 255, 0, 0, 1);//设置字体绘制的颜色
+    CGContextShowTextAtPoint(context, w/2-strlen(text)*5, h/2, text, strlen(text));//设置字体绘制的位置
+    //Create image ref from the context
+    CGImageRef imageMasked = CGBitmapContextCreateImage(context);//创建CGImage
+    CGContextRelease(context);
+    CGColorSpaceRelease(colorSpace);
+    return [UIImage imageWithCGImage:imageMasked];//获得添加水印后的图片   
+}
+
+
+
+-(UIImage *)addImageLogo:(UIImage *)img text:(UIImage *)logo
+{
+    //get image width and height
+    int w = img.size.width;
+    int h = img.size.height;
+    int logoWidth = logo.size.width;
+    int logoHeight = logo.size.height;
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    //create a graphic context with CGBitmapContextCreate
+    CGContextRef context = CGBitmapContextCreate(NULL, w, h, 8, 44 * w, colorSpace, kCGImageAlphaPremultipliedFirst);
+    CGContextDrawImage(context, CGRectMake(0, 0, w, h), img.CGImage);
+    CGContextDrawImage(context, CGRectMake(w-logoWidth, 0, logoWidth, logoHeight), [logo CGImage]);
+    CGImageRef imageMasked = CGBitmapContextCreateImage(context);
+    CGContextRelease(context);
+    CGColorSpaceRelease(colorSpace);
+    return [UIImage imageWithCGImage:imageMasked];
+    // CGContextDrawImage(contextRef, CGRectMake(100, 50, 200, 80), [smallImg CGImage]);
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
